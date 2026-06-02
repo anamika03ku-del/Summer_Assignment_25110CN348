459ad6520eb002d39edc89064ae05d5828cb01db
Anamika kumari <anamika03ku@.com>
2026-06-02 00:17:51 -0800

    Add day1 files

---
+++ day1/four.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class four {
+   public static void main(String[] args) {
+        
+        String k = "12345678";
+        System.out.println(k.length());
+        
+    }
+    
+}
