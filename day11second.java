c5f17e5a9b707c6166c8b398628121c3316f681c
Anamika kumari <anamika03ku@gmail.com>
2026-06-11 14:54:10 +0530

    Add files via upload

---
+++ day11second.java
@@ -0,0 +1,17 @@
+package day11;
+
+public class day11second {
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+    
+    public static void main(String[] args) {
+        int result = factorial(5);
+        System.out.println(result);
+    }
+}
+
+       
